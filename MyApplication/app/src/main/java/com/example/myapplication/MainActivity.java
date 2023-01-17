649acf1709c72054661751f3f6f449f481e75555
eshetu70 <eshetuwek1@gmail.com>
2023-01-17 15:34:53 -0500

    updated

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,15 +4,57 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.concurrent.BlockingDeque;
 
 public class MainActivity extends AppCompatActivity {
 
+    SeekBar seekBar;
+    TextView textViewProgress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Testing Toast", Toast.LENGTH_SHORT).show();
+
+        String TAG ="demo";
+        findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: button1");
+
+            }
+        });
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: button2");
+            }
+        });
+        seekBar =findViewById(R.id.seekBar);
+        textViewProgress =findViewById(R.id.textViewProgress);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                Log.d(TAG, "onProgressChanged: "+ i);
+                textViewProgress.setText(String.valueOf(i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Log.d(TAG, "onStartTrackingTouch: ");
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Log.d(TAG, "onStopTrackingTouch: ");
+
+            }
+        });
 
     }
 }
